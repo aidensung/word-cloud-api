8e7331ff29f68bf1b68f768b2fed9c2a3268059b
Ilhyo Sung <sih8189@gmail.com>
2020-03-21 04:16:27 -0400

    connected word cloud api with word cloud app and added detail figure setting

--- word_cloud.py
+++ word_cloud.py
@@ -3,8 +3,11 @@ import nltk
 from collections import Counter
 import matplotlib.pyplot as plt
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='outputs')
+CORS(app)
 
 font_path = 'SECRCODE.TTF'
 
@@ -37,12 +40,12 @@ def make_cloud_image(tags, file_name):
     fig.savefig("outputs/{0}.png".format(file_name))
 
 
-def process_from_text(text, max_count, min_length, words):
-    tags = get_tags(text, max_count, min_length)
+def process_from_text(text, max_count, min_length, words, file_name):
+    tags = get_tags(text, int(max_count), int(min_length))
     for n, c in words.items():
         if n in tags:
-            tags[n] = tags[n] * int(words[n])
-    make_cloud_image(tags, "output")
+            tags[n] = tags[n] * float(words[n])
+    make_cloud_image(tags, file_name)
 
 
 @app.route("/process", methods=['GET', 'POST'])
@@ -52,10 +55,28 @@ def process():
     if content['words'] is not None:
         for data in content['words'].values():
             words[data['word']] = data['weight']
-    process_from_text(content['text'], content['maxCount'], content['minLength'], words)
+    process_from_text(content['text'], content['maxCount'], content['minLength'], words, content['textID'])
     result = {'result':True}
     return jsonify(result)
 
 
+@app.route('/outputs', methods=['GET', 'POST'])
+def output():
+    text_id = request.args.get('textID')
+    return app.send_static_file(text_id + '.png')
+
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    text_id = request.args.get('textID')
+    path = "outputs/{0}.png".format(text_id)
+    result = {}
+    if os.path.isfile(path):
+        result['result'] = True
+    else:
+        result['result'] = False
+    return jsonify(result)
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    app.run('0.0.0.0', port=5000, threaded=True)
