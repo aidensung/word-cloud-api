e6140ad02d63b0771a062b24bc6ce4b9ec9b4db4
Ilhyo Sung <sih8189@gmail.com>
2020-04-08 09:42:51 -0400

    modified response headers

--- word_cloud.py
+++ word_cloud.py
@@ -12,9 +12,7 @@ CORS(app)
 
 @app.after_request
 def add_headers(response):
-    response.headers.add('Content-Type', 'application/json')
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
     return response
 
 
